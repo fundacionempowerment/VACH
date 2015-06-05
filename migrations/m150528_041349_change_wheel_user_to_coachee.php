e04db7a03a3b213b57c0f4d3d2be8da7c2469f20
Marcelo Briones <ing@marcelobriones.com.ar>
2015-06-05 00:25:59 -0300

    - Improves migrations

--- migrations/m150528_041349_change_wheel_user_to_coachee.php
+++ migrations/m150528_041349_change_wheel_user_to_coachee.php
@@ -5,11 +5,15 @@ use yii\db\Migration;
 class m150528_041349_change_wheel_user_to_coachee extends Migration {
 
     public function up() {
+        $this->dropForeignKey('fk_wheel_user', '{{%wheel}}');
         $this->renameColumn('{{%wheel}}', 'user_id', 'coachee_id');
+        $this->addForeignKey('fk_wheel_user', '{{%wheel}}', 'coachee_id', '{{%user}}', 'id', 'CASCADE');
     }
 
     public function down() {
+        $this->dropForeignKey('fk_wheel_user', '{{%wheel}}');
         $this->renameColumn('{{%wheel}}', 'coachee_id', 'user_id');
+        $this->addForeignKey('fk_wheel_user', '{{%wheel}}', 'user_id', '{{%user}}', 'id', 'CASCADE');
     }
 
 }
