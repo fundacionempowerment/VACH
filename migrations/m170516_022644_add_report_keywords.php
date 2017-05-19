ca7863bdd136d226202fd84977ffeda42b2bc379
Marcelo Briones <ing@marcelobriones.com.ar>
2017-05-18 22:33:00 -0300

    Fix migrations

--- migrations/m170516_022644_add_report_keywords.php
+++ migrations/m170516_022644_add_report_keywords.php
@@ -20,26 +20,27 @@ class m170516_022644_add_report_keywords extends Migration
         $this->addColumn('individual_report', 'emergents_keywords', $this->text()->null());
         $this->addColumn('individual_report', 'performance_keywords', $this->text()->null());
 
+        $this->execute('UPDATE `report` SET `introduction_keywords` = `summary`');
         $this->dropColumn('report', 'summary');
     }
 
     public function down()
     {
+        $this->addColumn('report', 'summary', $this->text()->null());
+        $this->execute('UPDATE `report` SET `summary` = `introduction_keywords`');
+
         $this->dropColumn('report', 'introduction_keywords');
         $this->dropColumn('report', 'effectiveness_keywords');
         $this->dropColumn('report', 'performance_keywords');
         $this->dropColumn('report', 'competences_keywords');
         $this->dropColumn('report', 'emergents_keywords');
         $this->dropColumn('report', 'relations_keywords');
-        $this->dropColumn('report', 'action_plan_keywords');
 
         $this->dropColumn('individual_report', 'perception_keywords');
         $this->dropColumn('individual_report', 'relations_keywords');
         $this->dropColumn('individual_report', 'competences_keywords');
         $this->dropColumn('individual_report', 'emergents_keywords');
         $this->dropColumn('individual_report', 'performance_keywords');
-
-        $this->addColumn('report', 'summary', $this->text()->null());
     }
 
 }
