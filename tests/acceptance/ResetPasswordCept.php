e6313cd87d34421b4764a4361bfd86d699982369
Marcelo Briones <ing@marcelobriones.com.ar>
2017-07-03 22:33:15 -0300

    Add reset password test

--- tests/acceptance/ResetPasswordCept.php
+++ tests/acceptance/ResetPasswordCept.php
@@ -1,13 +1,36 @@
 <?php
 
+$random = rand(111, 999);
+$email = "coach@example.com";
+$password = "password$random";
+
 $I = new AcceptanceTester($scenario);
-$I->wantTo('ensure that backup works');
+$I->wantTo('ensure that reset password works');
 
-$I->loginAsAdmin();
+$I->resetEmails();
 
-$I->clickMainMenu('Admin', 'Copia de seguridad');
-$I->wait(1);
+$I->amOnPage(Yii::$app->homeUrl);
 
-$I->waitForText('Backup sent!');
+$I->click('Reiniciar contraseña');
 
-$I->seeInLastEmailSubject("Backup");
+$I->waitForText('Solicitud de reinicio de contraseña', 30, 'h1');
+
+$I->fillField('PasswordResetRequestForm[email]', $email);
+$I->click('Solicitar');
+
+$I->waitForText('Revise su buzón de correo para más instrucciones');
+
+$I->seeInLastEmailSubject('Reinicio de contraseña para VACH');
+
+$link = $I->grabFromLastEmail("");
+
+$I->amOnUrl($link);
+
+$I->waitForText('Reiniciar contraseña', 30, h1);
+
+$I->fillField('ResetPasswordForm[password]', $password);
+$I->click('Guardar');
+
+$I->waitForText('Nueva contraseña guardada');
+
+$I->login('coach', $password);
