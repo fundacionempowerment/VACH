67a05554cb79ec0ea402b423f7a0475342e7dc40
Marcelo Briones <ing@marcelobriones.com.ar>
2018-06-26 21:40:40 -0300

    Updates tests

--- tests/acceptance/TeamTypeCept.php
+++ tests/acceptance/TeamTypeCept.php
@@ -8,8 +8,7 @@ $I->loginAsAdmin();
 $I->clickMainMenu('Admin','Tipos de equipos');
 $I->wait(1);
 
-$I->click("//*[@title = 'Edit']");
-$I->click("Guardar");
-$I->wait(1);
+$I->click("Duplicar");
+$I->acceptPopup();
 
-$I->see('Empresa ha sido exitosamente editado');
+$I->waitForText('(copy)');
