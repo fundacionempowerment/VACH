8e50ce33860711004e3fc46f72e939f59d4f054d
Marcelo Briones <ing@marcelobriones.com.ar>
2020-09-27 21:59:19 -0300

    Include script for fast docker image update

---
+++ docker/update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker pull ubuntu:bionic
+docker pull mysql:5.7
+docker pull phpmyadmin/phpmyadmin
+docker pull selenium/standalone-chrome-debug
+docker pull tophfr/mailcatcher
