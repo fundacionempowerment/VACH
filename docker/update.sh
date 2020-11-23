adb285cc7d1bfea91686a703661bfc5a26c4b113
Marcelo Briones <ing@marcelobriones.com.ar>
2020-11-23 14:33:44 -0300

    Upgrade SO

--- docker/update.sh
+++ docker/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-docker pull ubuntu:bionic
+docker pull ubuntu:focal
 docker pull mysql:5.7
-docker pull phpmyadmin/phpmyadmin
+docker pull phpmyadmin
 docker pull selenium/standalone-chrome-debug
 docker pull tophfr/mailcatcher
